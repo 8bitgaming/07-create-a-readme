85f56bb15b33559c80cae2b29bf4ee7825a27172
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-01-28 00:14:55 -0600

    asking questions using inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -4,13 +4,57 @@ const fs = require('fs')
 const generateMarkdown = require('../Develop/utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+answers = []
+const promptUser = async () => {
+
+    const readmeData = await inquirer.prompt(
+        [{
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?(Required)',
+            validate: nameInput_1 => {
+                if (nameInput_1) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide installation instructions:',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage instructions:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide testing instructions:',
+        }
+        ]);
+    answers.push(readmeData);
+    console.log(answers);
+};
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+// function init() {
+    
+// }
 
 // Function call to initialize app
-init();
+// init();
+promptUser()
