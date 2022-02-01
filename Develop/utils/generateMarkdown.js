8a6228cf0e772fe2f3b5d33209fdfa244cbac538
Jeremy Gieske <jeremy.gieske@gmail.com>
2022-02-01 01:19:23 -0600

    Added all entry options, fixed table of contents, fixed formatting and added a check for Dist folder

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,70 +1,127 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//set a global variable for some repeated text
+const noInfo = 'No information available.'
 
 const installationDisplay = (installation) => {
-if (installation) {return `
+if (installation) {
+  return `
 ## Installation instructions 
 ${installation}`
-  } else {return''}
+  }
+  return `
+## Installation instructions 
+${noInfo}`
 }
 
 const usageDisplay = (usage) => { usage 
-  if (usage) {return `
-  ## Usage instructions 
-  ${usage}`
-    } else {return''}
+  if (usage) {
+    return `
+## Usage instructions 
+${usage}`
+    }
+    return `
+## Usage instructions 
+${noInfo}`
   }
+
+  const contributingDisplay = (contributor) => {
+    if (contributor) {
+      return `
+## Contributing to this project
+Your contributions are welcome to this project! Feel free to fork this repository and submit pull requests with any contributions. For further information please see the questions section below.`
+      }
+      return `
+## Contributing to this project
+Contributing is not requested at this time.`
+    }
 
 const creditsDisplay = (credits) => {
-  if (credits) {return `
-  ## Additional Contributors
-  ${credits}`
-    } else {return''}
+  if (credits) {
+    return `
+## Credits
+${credits}`
+    }
+    return `
+## Credits
+${noInfo}`
   }
 
-  const licenseBadge = (license) => {
-    if (license !== 'None') {
-    let badge = ''
-    switch (license) {
-      case 'MIT' : badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-      break;
-      case 'GNU GPLv3' : badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-      break;
-      case 'Mozilla Public 2.0' : badge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
-      break;
-      case 'The Unlicense' : badge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
-      break;
-      case 'Apache 2.0' : badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
-      break;
-      default : badge = ''
-    }
-    return badge
-      } else {
-        return ''
+  const testingText = (tests) => {
+    if (tests) {
+      return `
+## Testing Instructions
+${tests}`
       }
+return `
+## Testing instructions 
+${noInfo}`
     }
 
-const licenseText= (license) => {
+const licenseBadge = (license) => {
   if (license !== 'None') {
-    return `## License
-    Licensed under the ${license} license.`
+  let badge = ''
+  switch (license) {
+    case 'MIT' : badge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    break;
+    case 'GNU GPLv3' : badge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    break;
+    case 'Mozilla Public 2.0' : badge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+    break;
+    case 'The Unlicense' : badge = `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+    break;
+    case 'Apache 2.0' : badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    break;
+    default : badge = ''
+  }
+  return badge
     } else {
       return ''
     }
+  }
+
+const licenseText= (license) => {
+  if (license !== 'None') {
+    return `
+## License
+Licensed under the ${license} license.`
+    } else {
+return `
+## License
+${noInfo}`
+    }
 }
 
-// generate markdown for README. Only the required fields are pre-populated.
+const questionText= (github, email) => {
+  let finalText = ''
+
+  if (github && email) {
+    const gitHubURL = `https://github.com/${github}`
+    const emailAddress = `[${email}](mailto:${email})`
+    finalText = `
+## Questions
+For further information, please check out my github repository at ${gitHubURL}, or by emailing ${emailAddress}`
+
+    } else if (github) {
+      const gitHubURL = `https://github.com/${github}`
+    finalText = `
+## Questions
+For further information, please check out my github repository at ${gitHubURL}`
+
+    } else if (email) {
+      const emailAddress = `[${email}](mailto:${email})`
+      finalText = `
+## Questions
+For further information, please email ${emailAddress}`
+
+    } else {`
+## Questions
+${noInfo}`
+    }
+return finalText
+    }
+
+// generate markdown for README.
 function generateMarkdown(data) {
-  const { title, description, installation, usage, credits, license} = data
+  const { title, description, installation, usage, contributor, credits, license, tests, github, email} = data
   
 return `
 ${licenseBadge(license)}
@@ -72,13 +129,24 @@ ${licenseBadge(license)}
 
 ## Description
 ${description}
+
+## Table of Contents
+* [Installation instructions ](#Installation-instructions)
+* [Usage instructions ](#Usage-instructions)
+* [Contributing to this project ](#Contributing-to-this-project)
+* [Credits](#Credits)
+* [License](#License)
+* [Testing Instructions](#Testing-Instructions)
+* [Questions](#Questions)
+
 ${installationDisplay(installation)}
 ${usageDisplay(usage)}
+${contributingDisplay(contributor)}
 ${creditsDisplay(credits)}
 ${licenseText(license)}
-
+${testingText(tests)}
+${questionText(github, email)}
 `
-;
-}
+};
 
 module.exports = generateMarkdown;
